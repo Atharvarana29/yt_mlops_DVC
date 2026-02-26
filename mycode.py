caf0bb999d7bef47977dccb2a4c40f13767b25c0
atharva <atharvarana29@gmail.com>
2026-02-26 23:03:00 +0530

    third version of data saved

--- mycode.py
+++ mycode.py
@@ -15,11 +15,13 @@ df = pd.DataFrame(data)
 new_row_loc = {'Name' : 'GF1' , 'Age' : 20 , 'city' : 'city1'}
 df.loc[len(df.index)]= new_row_loc
 
+# adding another new row to df for V3
+new_row_loc2 = {'Name': 'GF2' , 'Age': 30 , 'city':'City2'}
+df.loc[len(df.index)] = new_row_loc2
 
 # Ensure the "data" directory exists at root level
 data_dir = 'data'
 os.makedirs(data_dir , exist_ok= True) # True means if their is already a data that is with the same name then use that only , do not overwrite or replace it 
-
 
 
 # define the file path 
